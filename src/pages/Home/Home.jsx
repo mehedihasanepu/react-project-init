ee466057179a39d379c82a9df9b4eeb85ba767d6
mehedihasanepu <mehedihasanepu123@gmail.com>
2024-07-29 18:17:24 +0600

    react-project-init created

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div >
+            <p className="text-2xl font-bold text-center">Hello world..!!!</p>
+        </div>
+    );
+};
+
+export default Home;
+
